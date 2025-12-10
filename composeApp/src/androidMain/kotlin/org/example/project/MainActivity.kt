b4cc502029691598b396303bbfd93e1291508109
datpham0412 <tiendat041202@gmail.com>
2025-12-10 14:49:32 +1100

    some test to log the key out, make sure to pass the key successfully to app level

--- composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.shared.TestRunner
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        TestRunner().run(BuildConfig.GEMINI_API_KEY)
+        val provider = AndroidApiKeyProvider()
+        Log.d("KEY_TEST_ANDROID", "Key = '${provider.geminiKey}'")
+        TestRunner(provider).run()
+
         setContent {
             App()
         }
