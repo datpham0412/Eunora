2c7889e3e8d2da22dd009401f1a9c85b88145601
datpham0412 <tiendat041202@gmail.com>
2025-12-12 12:12:38 +1100

    remove usage of MainViewController (ios)

--- composeApp/src/iosMain/kotlin/org/example/project/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/example/project/MainViewController.kt
@@ -1,17 +1,7 @@
 package org.example.project
 
 import androidx.compose.ui.window.ComposeUIViewController
-import com.example.shared.ApiConfig
-import viewmodel.TestViewModel
 
 fun MainViewController() = ComposeUIViewController {
-    println("=================================")
-    println("GEMINI API KEY: ${ApiConfig.GEMINI_API_KEY}")
-    println("API KEY LENGTH: ${ApiConfig.GEMINI_API_KEY.length}")
-    println("=================================")
-
-    println("🧪 Starting ViewModel Test...")
-    TestViewModel.run()
-
     App()
 }
